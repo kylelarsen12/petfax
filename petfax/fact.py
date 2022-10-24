282bd14e5f8d4b643723feb35b6ca8470b995c65
unknown <kylelarsen12@gmail.com>
2022-10-24 18:25:12 -0400

    Implement beginning of post route for fact

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,14 @@
-from flask import ( Blueprint, render_template )
+from flask import ( Blueprint, render_template, request )
 
 bp = Blueprint("fact", __name__, url_prefix="/facts")
 
 @bp.route("/new")
 def new():
         return render_template("new.html")
+
+
+@bp.route("/", methods=["POST"])
+def index():
+        print(request.form)
+        return "Thanks for submitting a fun fact"
+
