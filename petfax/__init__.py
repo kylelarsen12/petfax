4f0fb1b4b18cd95663bce805024cabf7204c343b
unknown <kylelarsen12@gmail.com>
2022-10-24 17:55:46 -0400

    Implement fact and pet routes and styling

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -9,5 +9,8 @@ def create_app():
         
         from . import pet
         app.register_blueprint(pet.bp)
+
+        from . import fact
+        app.register_blueprint(fact.bp)
         
         return app
