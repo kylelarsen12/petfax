f9d2a7eca4704882b8cfe2430aedfefd0f18ab53
unknown <kylelarsen12@gmail.com>
2022-10-24 20:08:17 -0400

    Integrate db using flask_migrate

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
         app = Flask(__name__)
+
+        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Dcidrummer12@localhost:5432/petfax"
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
         @app.route("/")
         def hello():
@@ -13,4 +17,10 @@ def create_app():
         from . import fact
         app.register_blueprint(fact.bp)
         
+        from . import models
+        models.db.init_app(app)
+
+        migrate = Migrate(app, models.db)
+
         return app
+
